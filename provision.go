a06a9bf1c4ff8f41bd0834792f14fe5d825c16b3
Charles Dunbar <charles@puppet.com>
2018-03-07 10:36:57 -0600

    Able to create rolebinding in namespace

--- provision.go
+++ provision.go
@@ -10,14 +10,58 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 
-	"k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 var options struct {
 	Verbose    []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
-	Namespace  string `short:"n" long:"namespace" description:"Namespace to create" required:"true"`
+	Namespace  string `short:"n" long:"namespace" description:"Namespace to create"`
 	Kubeconfig string `short:"k" long:"kubeconfig" description:"absolute path to the kubeconfig file (default: ~/.kube/config)"`
+	Username   string `short:"u" long:"username" description:"User to add to namespace" required:"true"`
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
+
+func createNamespace(clientset *kubernetes.Clientset) {
+	nsSpec := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: options.Namespace}}
+
+	_, err := clientset.CoreV1().Namespaces().Create(nsSpec)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Created namespace %s\n", options.Namespace)
+}
+
+func createRoleBinding(clientset *kubernetes.Clientset) {
+	rbSpec := &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      options.Username + "-rolebinding",
+			Namespace: options.Namespace,
+		},
+		Subjects: []rbacv1.Subject{
+			rbacv1.Subject{
+				Name: options.Username,
+				Kind: "User",
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			Kind: "ClusterRole",
+			Name: "admin",
+		},
+	}
+
+	_, err := clientset.RbacV1().RoleBindings(options.Namespace).Create(rbSpec)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Created rolebinding %s-rolebinding in namespace %s", options.Username, options.Namespace)
 }
 
 func main() {
@@ -39,18 +83,7 @@ func main() {
 		panic(err.Error())
 	}
 
-	nsSpec := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: options.Namespace}}
+	createNamespace(clientset)
+	createRoleBinding(clientset)
 
-	_, err = clientset.CoreV1().Namespaces().Create(nsSpec)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("Created namespace %s\n", options.Namespace)
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
 }
